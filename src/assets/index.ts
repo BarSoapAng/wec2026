e95cdfa78c287cce18a9e9381040991f10399fbb
Angela <angela.zhou12345@gmail.com>
2026-06-10 11:05:09 -0400

    add more assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,3 +6,19 @@ export { default as Consulting } from "./about/consulting.jpg";
 export { default as JuniorDesign } from "./about/junior.jpg";
 export { default as Programming } from "./about/programming.jpg";
 export { default as ReEngineering } from "./about/reeng.jpg";
+
+// FAQ Section
+export { default as Towels } from "./faq/towels.png";
+
+// Hero Section
+export { default as PurpleSign } from "./hero/purple-sign.png";
+export { default as Umbrella } from "./hero/umbrella.png";
+export { default as WoodSign } from "./hero/wood-sign.png";
+
+// Schedule Section
+export { default as BoardSign } from "./schedule/board-sign.png";
+
+// Sponsors Section
+export { default as SponsorsBlue } from "./sponsors/sponsors-blue.png";
+export { default as SponsorsPurple } from "./sponsors/sponsors-purple.png";
+export { default as SponsorsTeal } from "./sponsors/sponsors-teal.png";
